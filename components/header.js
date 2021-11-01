b791b3a46e6ea406dd8789437b863ba6fc4f9492
Demaloo <demaloo.dev@gmail.com>
2021-11-01 14:06:22 +0600

    ch

--- components/header.js
+++ components/header.js
@@ -1,21 +1,31 @@
 import { Box, Container, Flex, Link } from "@chakra-ui/react";
+import { useViewportScroll } from "framer-motion";
 import NextLink from "next/link";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+	const [y, setY] = useState(0);
+	const { scrollY } = useViewportScroll();
+
+	useEffect(() => {
+		return scrollY.onChange(() => setY(scrollY.get()));
+	}, [scrollY]);
+
 	return (
 		<Box
 			as="header"
 			position="sticky"
 			top="0"
 			zIndex="1"
-			backgroundColor="transparent"
+			backgroundColor={y > 0 ? "white" : "transparent"}
 			height="3rem"
+			transition="background-color 0.4s"
 		>
 			<Container maxW="container.xl" height="100%">
 				<Flex height="100%" alignItems="center">
 					<NextLink href="/" passHref>
 						<Link
-							color="white"
+							color={y > 0 ? "gray.700" : "white"}
 							fontWeight="bold"
 							fontSize="xl"
 							_hover={{
